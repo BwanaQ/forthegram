6d471525c4b7480baec88b1f94db7fab6ab63cf2
BwanaQ <thunjawax@gmail.com>
2021-03-24 17:21:53 +0300

    Final starter template

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,34 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+posts = [
+    {
+        'author': 'BwanaQ',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2020'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'November 27, 2021'
+    },
+    {
+        'author': 'Loise Hunja',
+        'title': 'Blog Post 3',
+        'content': 'Third post content',
+        'date_posted': 'March 2, 2021'
+    }
+]
+
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
