17d054fa219e646cc7fe743d165fb82df32e94e9
BwanaQ <thunjawax@gmail.com>
2021-03-29 11:02:26 +0300

    add natural time to template

--- blog/views.py
+++ blog/views.py
@@ -1,35 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Image, Comment
-
-posts = [
-    {
-        'id': 1,
-        'author': 'BwanaQ',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2020'
-    },
-    {
-        'id': 2,
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'November 27, 2021'
-    },
-    {
-        'id': 3,
-        'author': 'Loise Hunja',
-        'title': 'Blog Post 3',
-        'content': 'Third post content',
-        'date_posted': 'March 2, 2021'
-    }
-]
+from datetime import datetime
 
 
 def home(request):
+    now = datetime.now()
     context = {
-        'posts': Image.objects.all()
+        'posts': Image.objects.all(),
+        'now': now
     }
     return render(request, 'blog/home.html', context)
 
