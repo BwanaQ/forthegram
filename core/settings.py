0d30aea87405a3e269af3d14dd450f10dca903b5
BwanaQ <thunjawax@gmail.com>
2021-03-29 12:09:41 +0300

    add flash messaging to base template

--- core/settings.py
+++ core/settings.py
@@ -39,10 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+
     # 3rd party apps
     'cloudinary',
+
     # my apps
     'blog',
+    'users',
 ]
 
 MIDDLEWARE = [
