34337d0cd94143ca9c15a8ba0fa0702a183b1fe3
BwanaQ <thunjawax@gmail.com>
2021-03-30 19:18:55 +0300

    final crud for Images

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from .views import (
     ImageListView,
     ImageDetailView,
-    ImageCreateView
+    ImageCreateView,
+    ImageUpdateView,
+    ImageDeleteView
 )
 from . import views
 
@@ -10,5 +12,7 @@ urlpatterns = [
     path('', ImageListView.as_view(), name='blog-home'),
     path('image/<int:pk>/', ImageDetailView.as_view(), name='image-detail'),
     path('image/new/', ImageCreateView.as_view(), name='image-create'),
+    path('image/<int:pk>/update/', ImageUpdateView.as_view(), name='image-update'),
+    path('image/<int:pk>/delete/', ImageDeleteView.as_view(), name='image-delete'),
     path('about/', views.about, name='blog-about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
     ListView,
     DetailView,
-    CreateView
+    CreateView,
+    UpdateView,
+    DeleteView
 )
 from django.http import HttpResponse
 from .models import Image, Comment
-
-
-def home(request):
-    context = {
-        'posts': Image.objects.all()
-    }
-    return render(request, 'blog/home.html', context)
 
 
 class ImageListView(ListView):
@@ -26,13 +22,39 @@ class ImageDetailView(DetailView):
     model = Image
 
 
-class ImageCreateView(CreateView):
+class ImageCreateView(LoginRequiredMixin, CreateView):
     model = Image
     fields = ['title', 'caption', 'url']
 
     def form_valid(self, form):
         form.instance.creator = self.request.user
         return super().form_valid(form)
+
+
+class ImageUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Image
+    fields = ['title', 'caption', 'url']
+
+    def form_valid(self, form):
+        form.instance.creator = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        image = self.get_object()
+        if self.request.user == image.creator:
+            return True
+        return False
+
+
+class ImageDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Image
+    success_url = '/'
+
+    def test_func(self):
+        image = self.get_object()
+        if self.request.user == image.creator:
+            return True
+        return False
 
 
 def about(request):
